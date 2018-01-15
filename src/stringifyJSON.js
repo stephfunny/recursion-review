060537b299e7de361e787caff3150031d82ffda2
stephfunny <contactstephanief@gmail.com>
2018-01-15 12:15:38 -0800

    implement stringifyJSON and passed all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //number, string, boolean, null, undefined, function
+  // array, obj
+  if (typeof(obj) === 'number') {
+    return String(obj);
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof(obj) === 'boolean') {
+    return String(obj);
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    return '[' + obj.map( function(element) {
+      return stringifyJSON(element);
+    }).join(',') + ']';
+  } else if (obj === undefined || typeof(obj) === 'function') {
+    return '';
+  } else {
+    var pairs = [];
+    Object.keys(obj).forEach(function (key) {
+      var val = stringifyJSON(obj[key]);
+      if (val) {
+        pairs.push('"' + key + '":' + stringifyJSON(obj[key]));  
+      }
+    });
+      
+    return '{' + pairs.join(',') + '}';
+  }
+
+
 };
